d0fe43c7ce3bf60cc12235e20eb9009b9ecab113
Va1k <andreas@valkyr.com.au>
2013-05-27 14:26:44 +1000

    Comments, comments, comments!
    They were there all along, I swear!

--- src/City.java
+++ src/City.java
@@ -8,45 +8,54 @@ import java.util.TimeZone;
  * Author: Andreas
  * Date  : 18/05/13
  * Time  : 2:57 PM
+ *
+ * City class for 'city' objects.
+ *
+ * Does a bulk of the work throughout the application.
  */
 public class City {
-    private String country;
+    private String region;
     private String name;
     private TimeZone timezone;
 
-    public City(String country, String name, TimeZone timezone) {
-        //lel
-        this.country = country;
+    // City constructor
+    public City(String region, String name, TimeZone timezone) {
+        this.region = region;
         this.name = name;
         this.timezone = timezone;
     }
 
-    public String getCountry() {
-        return this.country;
+    // Returns the location's region
+    public String getRegion() {
+        return this.region;
     }
 
+    // Returns the location's name
     public String getName() {
         return this.name;
     }
 
+    // Returns the timezone
     public TimeZone getTimezone() {
         return this.timezone;
     }
 
+    // Converts time
     public String[] convertTime() {
         // Finds out what the current time/date is.
         Date currentTime = new Date();
 
-        // Creates a date format
+        // Creates date formats for use
         DateFormat date = new SimpleDateFormat("dd, MMMM, yyyy");
         DateFormat normal = new SimpleDateFormat("hh:mm:ssa");
         DateFormat military = new SimpleDateFormat("HH:mm:ss");
+
+        // Sets the timezone of the date formats to this' (the object's timezone)
         date.setTimeZone(this.timezone);
         normal.setTimeZone(this.timezone);
         military.setTimeZone(this.timezone);
 
-        // Formats the date in the currently selected city's timezone
-        // Also returns both the normal & military formatted times in an array.
+        // Returns both the normal & military formatted times as well as the date in an array.
         return new String[]{date.format(currentTime), normal.format(currentTime), military.format(currentTime)};
     }
 
--- src/Tardis.java
+++ src/Tardis.java
@@ -29,41 +29,56 @@ public class Tardis extends Applet implements Runnable{
         setLayout(new BorderLayout());
         readDatabase();
 
+        // Panel for combo-boxes and other controls.
         Panel controls = new Panel(new BorderLayout());
         controls.setBackground(Color.decode("#FAFAFA"));
         controls.add(comboRegion, BorderLayout.NORTH);
         controls.add(comboLocation,BorderLayout.CENTER);
+        Button info = new Button("?");
+        controls.add(info, BorderLayout.EAST);
         add(controls, BorderLayout.NORTH);
 
+        // Small label down the bottom to tell the user what their timezone is.
         Label userTimeZone = new Label (" Your time zone: " + TimeZone.getDefault().getDisplayName() + " | " + TimeZone.getDefault().getID());
         userTimeZone.setForeground(Color.decode("#EEEEEE"));
         add(userTimeZone, BorderLayout.SOUTH);
 
-        Button info = new Button("?");
-        controls.add(info, BorderLayout.EAST);
-
+        // Runs the collection methods for the first time to populate the applet with data.
         getCountries();
         getCities();
         getCity();
 
+        /**
+         * Action listener in a subclass for the region combo-box.
+         *
+         * Calls:
+         *      getCities() - populates the location combo-box with cities within the selected country.
+         *      getCity() - The first city is already selected, now use the getCity method to obtain the city's variables.
+         */
         comboRegion.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent itemEvent) {
                 getCities();
-                comboLocation.select(0);
                 getCity();
             }
         });
 
+        /**
+         * Action listener in a subclass for the location combo-box.
+         *
+         * Calls:
+         *      getCity() - Whenever the location combo-box is changed, get the new city's variables.
+         */
         comboLocation.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent itemEvent) {
                 getCity();
             }
         });
-    }
+    } // End of init.
 
     public void start() {
+        // When the applet is started, if the animator thread doesn't exist, create it and start it running.
         if (animator == null) {
             animator = new Thread(this);
             animator.start();
@@ -71,12 +86,23 @@ public class Tardis extends Applet implements Runnable{
     } // End of start
 
     public void stop() {
+        // When the applet is stopped, if the animator exists, destroy it.
         if (animator != null) {
             animator = null;
         }
     } // End of Stop
 
     public void run() {
+        /**
+         * The run() method is called and managed by the 'animator' thread.
+         *
+         * While the animator is running
+         *      If a city has been selected
+         *          Grab the date, normal time and military time using the object's convertTime() method
+         *          Call the repaint(); method to refresh the applet.
+         *
+         * Then do nothing for 250ms.
+         */
         while(animator.isAlive()) {
             if(selectedCity != null) {
                 date = selectedCity.convertTime()[0];
@@ -94,22 +120,26 @@ public class Tardis extends Applet implements Runnable{
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Make some fonts
+        // Define some fonts.
         Font header = new Font("Trebuchet MS",Font.BOLD, 35);
         Font normal = new Font("Trebuchet MS",Font.PLAIN, 25);
         Font timezone = new Font("Trebuchet MS",Font.PLAIN,25);
 
+        // To prevent errors the first time the program is run, end the method if there isn't a city selected.
         if(selectedCity == null) {
             return;
         }
 
+        // Header, typically the country.
         g2.setFont(header);
-        g2.drawString(selectedCity.getCountry() + ", " + selectedCity.getName(), 10, 90);
+        g2.drawString(selectedCity.getRegion() + ", " + selectedCity.getName(), 10, 90);
 
+        // Timezone of the selected city.
         g2.setFont(timezone);
         g2.setColor(Color.decode("#AAAAAA"));
         g2.drawString(selectedCity.getTimezone().getDisplayName(),10, 120);
 
+        // The date, normal time & military time of the selected country.
         g2.setFont(normal);
         g2.setColor(Color.black);
         g2.drawString(date,10,150);
@@ -119,31 +149,32 @@ public class Tardis extends Applet implements Runnable{
     } // End of paint
 
     public void readDatabase() {
+        // Gets the relative URL to the .csv file with countries, etc entered into it.
         URL database = this.getClass().getResource("Database.csv");
-        /**
-         * Creates a new CSVReader via the OpenCSV package http://opencsv.sourceforge.net/
-         */
+
+        // Creates a new CSVReader object via the OpenCSV package http://opencsv.sourceforge.net/
         CSVReader csvReader = null;
         try {
             csvReader = new CSVReader(new FileReader(database.getPath()));
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Error handling if the file doesn't exist.
         }
 
         /**
-         * Slight bit of magic.
-         *
-         * Creates an 'ArrayList' of cities that is populated by a while loop with all the cities from the .csv file.
+         * Creates an 'ArrayList' of cities that is populated from the .csv file via a while loop.
          *
          * Now that we effectively have an array full of cities and their details the rest is pretty simple.
          */
+
+        // Creates a string array, as the .csv file is incremented through the array is populated with the cells in the row.
         String[] row;
+
         try {
-            while((row = csvReader.readNext()) != null) {
-                cities.add(new City(row[0],row[1],TimeZone.getTimeZone(row[3])));
+            while((row = csvReader.readNext()) != null) { // While there are still rows
+                cities.add(new City(row[0],row[1],TimeZone.getTimeZone(row[3]))); // Add a new city object to the cities ArrayList, create it by using data from the .csv
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Error handling if it can't read the file.
         }
 
         /**
@@ -152,28 +183,47 @@ public class Tardis extends Applet implements Runnable{
         try {
             csvReader.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace();  // Error handling
         }
     } // End of readDatabase()
 
     public void getCountries() {
-        ArrayList<String> addedCountries = new ArrayList<String>();
+        /**
+         * It's likely that locations will share a region. It doesn't really make sense to have the same region more than once in the drop-down list.
+         *
+         * So I made an ArrayList which holds regions that have already been added.
+         *
+         * A for loop then iterates through all the city objects and if the AddedRegions doesn't already contain it, it adds each city's region to the region drop-down list.
+         */
+        ArrayList<String> addedRegions = new ArrayList<String>();
         for (City s : cities) {
-            if(!addedCountries.contains(s.getCountry())) {
-                comboRegion.add(s.getCountry());
-                addedCountries.add(s.getCountry());
+            if(!addedRegions.contains(s.getRegion())) {
+                comboRegion.add(s.getRegion());
+                addedRegions.add(s.getRegion());
             }
         }
     } // End of getCountries()
 
     public void getCities() {
+        // First clear out all the locations currently in the drop-down list.
+        // This needs to be done because if there's a region change the previously selected region's locations would be left behind.
         comboLocation.removeAll();
+        /**
+         * Iterates through all the city objects, if a city's region variable matches the selected region it's added to the location drop-down list.
+         */
         for (City s : cities)
-            if (s.getCountry().equals(comboRegion.getSelectedItem()))
+            if (s.getRegion().equals(comboRegion.getSelectedItem()))
                 comboLocation.add(s.getName());
     } // End of getCities()
 
     public void getCity() {
+        /**
+         * It's important to actually find the city object so its relevant methods can be used
+         *
+         * All the cities are iterated through in order to find the actual city object that's selected.
+         *
+         * That city object is then put into the empty selectedCity 'City' object which is used throughout the rest of the code, particularly paint().
+         */
         for (City s : cities)
             if (s.getName().equals(comboLocation.getSelectedItem()))
                 selectedCity = s;
